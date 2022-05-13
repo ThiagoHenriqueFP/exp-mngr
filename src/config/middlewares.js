21fb43ee94712cb99326d02232c45eccf1a8e94b
Thiago Henrique Fonseca pereira <thiagop070@gmail.com>
2022-05-13 16:50:36 -0300

    Full delete from app

--- src/config/middlewares.js
+++
@@ -1,8 +0,0 @@
-const bodyParser = require('body-parser');
-
-module.exports = (app) => {
-
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-
-}
--- src/controllers/MensalExpenseController.js
+++
@@ -1,4 +0,0 @@
-module.exports = (app) => {
-  const { User, MensalExpense } = app.models;
-
-};
