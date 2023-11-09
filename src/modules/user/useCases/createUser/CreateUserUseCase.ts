d83a114819753f50d62f2839de93a8401480f759
Thiago Henrique <thiagohp070@gmail.com>
2023-11-09 09:39:27 -0300

    fix: create user with only lowcase strings

--- src/modules/user/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/user/useCases/createUser/CreateUserUseCase.ts
@@ -5,6 +5,9 @@ class CreateUserUseCase {
   constructor(private userRepository: UserRepository) { }
 
   async execute({ name, email, wage, password }: ICreate) {
+    name = name.toLowerCase();
+    email = email.toLowerCase();
+
     const userAlreadyExists = await this.userRepository.getByEmail(email);
 
     if (userAlreadyExists) throw new Error(`User with this email (${email}) already exists`);
